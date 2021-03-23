7233a353cbc14476a872be0de7979e54889934d0
nikki-ai <shony1198@gmail.com>
2021-03-23 13:56:14 -0500

    spaced login and sign up

--- src/routes/RegistrationRoute/RegistrationRoute.js
+++ src/routes/RegistrationRoute/RegistrationRoute.js
@@ -19,7 +19,7 @@ class RegistrationRoute extends Component {
         <p>
           Practice learning a language with the spaced repetition technique.
         </p>
-        <h2>Sign up</h2>
+        {' '}<h2>Sign up</h2>
         <RegistrationForm
           onRegistrationSuccess={this.handleRegistrationSuccess}
         />
