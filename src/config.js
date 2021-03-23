a92cc3d8f8f97bda8948efb4c4822dda3b340eb7
nikki-ai <shony1198@gmail.com>
2021-03-23 12:10:16 -0500

    removed s from http config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://powerful-earth-42983.herokuapp.com/api',
+  API_ENDPOINT: 'http://powerful-earth-42983.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
